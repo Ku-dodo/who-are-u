1b0cdef4e8abd805a247f5efbf48920e60161c56
Ku-dodo <rnehgus9403@gmail.com>
2023-11-01 14:56:01 +0900

    Commit Hardscene
    Commit Hardscene

--- Assets/Scripts/BtnEvent.cs
+++ Assets/Scripts/BtnEvent.cs
@@ -1,20 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BtnEvent : MonoBehaviour
 {
+    public GameObject stageBtnPanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     //start 씬 로드
@@ -22,9 +24,34 @@ public class BtnEvent : MonoBehaviour
     {
         SceneManager.LoadScene("StartScene");
     }
-    //main 씬 로드
+    //4*4 씬 로드
     public void LoadMainScene()
     {
         SceneManager.LoadScene("MainScene");
+    }
+    //6*6 씬 로드
+    public void LoadHardScene()
+    {
+        SceneManager.LoadScene("HardScene");
+    }
+
+    void BtnInteractive()
+    {
+        //시작 씬인지 아닌지 확인하기 위한 용도
+        if (SceneManager.GetActiveScene().name == "StartScene")
+        {
+            Button[] StageBtn = stageBtnPanel.transform.GetComponentsInChildren<Button>(); 
+            int ClearStage = PlayerPrefs.GetInt("ClearStage");
+            //버튼 초기화
+            foreach (Button button in StageBtn)
+            {
+                button.interactable = false;
+            }
+            //버튼 해금
+            for (int i = 0; i <= ClearStage; i++)
+            {
+                StageBtn[i].interactable = true;
+            }
+        }
     }
 }
